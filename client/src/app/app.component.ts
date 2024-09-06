cc74a18866563a1a922cf5b4adaafde00719beae
Flavian Maina <FLAVIAN.WAMBETI@equitybank.co.ke>
2024-09-06 16:15:39 +0300

    folder-reset

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,16 +8,18 @@ import { fontAwesomeIcons } from './shared/font-awesome-icons';
 import { ButtonModule } from 'primeng/button';
 import { NavbarComponent } from "./layout/navbar/navbar.component";
 import { FooterComponent } from "./layout/footer/footer.component";
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, ButtonModule, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, FontAwesomeModule,ButtonModule, NavbarComponent, FooterComponent, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   faIconLibrary = inject(FaIconLibrary);
+  isListingView: boolean = true;
 
   ngOnInit(): void {
     this.initFontAwesome();
